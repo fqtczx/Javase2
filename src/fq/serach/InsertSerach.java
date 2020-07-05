dcd195a293eae58a179c87d6203c184b75918b58
fqtczx <2574326165@qq.com>
2020-07-05 20:19:52 +0800

    第一次提交

--- src/fq/serach/InsertSerach.java
+++ src/fq/serach/InsertSerach.java
@@ -9,9 +9,11 @@ public class InsertSerach {
     public static int insertSer(int[] arr,int left,int right,int val){
         int mid=left+(val-arr[left])*(right-left)/(arr[right]-arr[left]);
         //说明已经找完整个数组了，但是还没有找到，就返回-1
-        if(left>right){
+        //后面两个判断条件是必须的，否则容易越界
+        if(left>right || val>arr[arr.length-1] || val<arr[0]){
             return -1;
         }
+
         //分别进行左右递归查找数组的下标
         if(arr[mid]==val){
             return mid;

756957a28ac6cb855fa220fad62130a18b5cebd7
fqtczx <2574326165@qq.com>
2020-07-13 20:55:44 +0800

    二叉树

--- src/fq/hashtable/HashTableDemo.java
+++ src/fq/hashtable/HashTableDemo.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class HashTableDemo {
     public static void main(String[] args) {
-        HashTable ht=new HashTable(7);
+        HashTable ht=new HashTable(7);//初始化，共有7条链表
         String key="";
         Scanner sc=new Scanner(System.in);
         System.out.println();
@@ -13,7 +13,7 @@ public class HashTableDemo {
             System.out.println("list:列表");
             System.out.println("find:查找");
             System.out.println("add:退出");
-            key=sc.next();
+            key=sc.next();//读取键盘字符
             switch (key){
                 case "add":
                     System.out.println("请输入id");
@@ -21,18 +21,18 @@ public class HashTableDemo {
                     System.out.println("请输入姓名");
                     String name=sc.next();
                     ht.add(new Emp(id,name));
-                    break;
+                    break;//注意这里的break
                 case "list":
                     ht.list();
                     break;
-                case "exit":
-                    sc.close();
-                    System.exit(0);
                 case "find":
                     System.out.println("请输入id");
                     int findid=sc.nextInt();
                     ht.find(findid);
                     break;
+                case "exit":
+                    sc.close();
+                    System.exit(0);
                 default:
                     break;
             }
@@ -41,31 +41,38 @@ public class HashTableDemo {
 }
 
 class HashTable{
+    //其底层是存储EmpLinkedList的数组
     private EmpLinkedList[] empls;
-    int size;
+    int size;//给定数组的大小
 
+    //构造方法，这里传入了大小的参数
     public HashTable(int size){
         this.size=size;
-        empls=new EmpLinkedList[size];
+        empls=new EmpLinkedList[size];//这里要为每一个EmpLinkedList初始化，不然
+        //会造成空指针异常，遍历初始化
         for(int i=0;i<empls.length;i++){
             empls[i]=new EmpLinkedList();
         }
     }
 
+    //添加方法，这里用到了散列函数，通过id决定将雇员放在那一条链表中
     public void add(Emp emp){
         empls[hashFun(emp.id)].add(emp);
     }
 
+    //散列函数，通过取模决定
     public int hashFun(int id){
         return id%size;
     }
 
+    //遍历方法，取出每条链表，通过链表的list()来实现功能
     public void list(){
         for(int i=0;i<size;i++){
             empls[i].list(i);
         }
     }
 
+    //查找方法，也是通过链表的方法实现的
     public void find(int id){
         Emp findemp=empls[hashFun(id)].find(id);
         if(findemp!=null){
@@ -78,9 +85,13 @@ class HashTable{
     }
 }
 
+//此类通过链表的方式存储id符合某些条件的雇员
 class EmpLinkedList{
-    private Emp head;
+    private Emp head;//头节点，默认为空
 
+    //添加雇员的方法，考虑头节点为空的情况
+    //假定，当添加雇员时，id是自增长的，即id的分配总是从小到大
+    //因此我们将雇员直接加到本链表的最后即可
     public void add(Emp emp){
         if(head==null){
             head=emp;
@@ -93,6 +104,8 @@ class EmpLinkedList{
         }
     }
 
+    //打印此条链表中含有的雇员信息
+    //这里传入了第几条EmpLinkedList的参数
     public void list(int no){
         if(head==null){
             System.out.println("第"+(no+1)+"条链表为空");
@@ -107,6 +120,7 @@ class EmpLinkedList{
         }
     }
 
+    //通过id来查找雇员信息，遍历查找
     public Emp find(int id){
         if(head==null){
             System.out.println("链表为空");
@@ -123,16 +137,19 @@ class EmpLinkedList{
     }
 }
 
+//表示一个雇员
 class Emp{
     public int id;
     public String name;
     public Emp next;
 
+    //构造方法
     public Emp(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    //重写toString()方法
     @Override
     public String toString() {
         return "Emp{" +
--- src/fq/tree/BinaryTreeDemo.java
+++ src/fq/tree/BinaryTreeDemo.java
@@ -2,6 +2,36 @@ package fq.tree;
 
 public class BinaryTreeDemo {
     public static void main(String[] args) {
+        BinaryTree bt=new BinaryTree();
+
+        //需要创建的节点
+        HeroNode root=new HeroNode(1,"宋江");
+        HeroNode h2=new HeroNode(2,"吴用");
+        HeroNode h3=new HeroNode(3,"卢俊义");
+        HeroNode h4=new HeroNode(4,"林冲");
+        HeroNode h5=new HeroNode(5,"关胜");
+        bt.setRoot(root);
+
+        //先手动创建二叉树，之后通过递归的方式构建二叉树
+        root.setLeft(h2);
+        root.setRight(h3);
+        h3.setRight(h4);
+        h3.setLeft(h5);
+        //二叉树的前序遍历
+        System.out.println("二叉树的前序遍历结果为：");
+        bt.preOrder();
+        //二叉树的中序遍历
+        System.out.println("二叉树的中序遍历结果为：");
+        bt.infixOrder();
+        //二叉树的后序遍历
+        System.out.println("二叉树的后序遍历结果为：");
+        bt.postOrder();
+
+        //前中后序遍历查找
+        System.out.println("前中后序遍历查找结果");
+        System.out.println(bt.preFind(1).toString());
+        System.out.println(bt.infixFind(2).toString());
+        System.out.println(bt.postFind(2).toString());
 
     }
 }
@@ -13,6 +43,7 @@ class BinaryTree{
         root=hn;
     }
 
+    //前序遍历
     public void preOrder(){
         if(root!=null){
             root.preOrder();
@@ -36,20 +67,43 @@ class BinaryTree{
             System.out.println("二叉树为空，无法遍历");
         }
     }
-    public void add(HeroNode hn){
-        if(root==null){
-            root=hn;
-        }else{
 
+    //前序遍历查找
+    public HeroNode preFind(int no){
+        if(root != null){
+            return root.preFind(no);
+        }else{
+            return null;
+        }
+    }
+
+    //中序遍历查找
+    public HeroNode infixFind(int no){
+        if(root != null){
+            return root.infixFind(no);
+        }else{
+            return null;
+        }
+    }
+
+    //后序遍历查找
+    public HeroNode postFind(int no){
+        if(root != null){
+            return root.postFind(no);
+        }else{
+            return null;
         }
     }
 }
+
+//构建节点信息
 class HeroNode{
     private int no;
     private String name;
-    private HeroNode left;
-    private HeroNode right;
+    private HeroNode left;//左子节点
+    private HeroNode right;//右子节点
 
+    //构造方法
     public HeroNode(int no, String name) {
         this.no = no;
         this.name = name;
@@ -95,6 +149,7 @@ class HeroNode{
                 '}';
     }
 
+    //递归实现前序遍历
     public void preOrder(){
         System.out.println(this.toString());
         if(left!=null){
@@ -105,6 +160,7 @@ class HeroNode{
         }
     }
 
+    //递归实行中序遍历
     public void infixOrder(){
         if(left!=null){
             this.left.infixOrder();
@@ -115,6 +171,7 @@ class HeroNode{
         }
     }
 
+    //递归实现后序遍历
     public void postOrder(){
         if(left!=null){
             this.left.postOrder();
@@ -123,6 +180,78 @@ class HeroNode{
             this.right.postOrder();
         }
         System.out.println(this.toString());
+    }
 
+    //前序查找
+    public HeroNode preFind(int no){
+        System.out.print("q");
+        if(this.no==no){
+            return this;
+        }
+
+        //则判断当前节点的左节点是否为空，如果不为空，则递归前序查找
+        //如果左递归前序查找找到，则返回
+        HeroNode res=null;//这个变量很重要，用来记录找到的节点，并返回
+        if(this.left!=null){
+            res=this.left.preFind(no);
+        }
+        if(res!=null){
+            return res;
+        }
+
+        //则判断当前节点的右节点是否为空，如果不为空，则递归前序查找
+        //如果右递归前序查找找到，则返回
+        if(this.right!=null){
+            res=this.right.preFind(no);
+        }
+        return res;
+    }
+
+    //中序遍历查找
+    public HeroNode infixFind(int no){
+        HeroNode res=null;//这个变量很重要，用来记录找到的节点，并返回
+        if(this.left!=null){
+            res=this.left.infixFind(no);
+        }
+
+        if(res!=null){
+            return res;
+        }
+
+        System.out.print("z");
+        if(this.no==no){
+            return this;
+        }
+
+        if(this.right!=null){
+            res=this.right.infixFind(no);
+        }
+        return res;
+    }
+
+    //后序遍历查找
+    public HeroNode postFind(int no){
+        HeroNode res=null;//这个变量很重要，用来记录找到的节点，并返回
+        if(this.left!=null){
+            res=this.left.postFind(no);
+        }
+
+        if(res!=null){
+            return res;
+        }
+
+        if(this.right!=null){
+            res=this.right.postFind(no);
+        }
+
+        if(res!=null){
+            return res;
+        }
+
+        System.out.print("h");
+        if(this.no==no){
+            return this;
+        }
+        return res;
     }
 }
